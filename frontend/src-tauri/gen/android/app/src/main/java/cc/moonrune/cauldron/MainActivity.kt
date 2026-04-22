0df09541495cf36d658a092fa178f79d9326e1c3
Maya Kade <maya@moonrune.cc>
2026-04-22 15:46:07 -0400

    feat(tauri): generate Android project via tauri android init
    Generate gen/android/ for Tauri v2 Android builds.
    Includes gradle wrapper, app manifest, and launcher icons.
    
    Signed-off-by: Maya Kade <maya@moonrune.cc>

---
+++ frontend/src-tauri/gen/android/app/src/main/java/cc/moonrune/cauldron/MainActivity.kt
@@ -0,0 +1,11 @@
+package cc.moonrune.cauldron
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
